7194919742c9134a1e34750eb39641694eb29ca2
jokyo02 <149650929+jokyo02@users.noreply.github.com>
2023-11-30 14:30:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
+// 引入express模块
 const express = require('express');
-const httpProxy = require('http-proxy');
-
-// 定义反向代理的目标 URL
-const targetUrl = 'https://bing.cf03-b29.workers.dev';
-
-// 创建反向代理服务器
-const proxy = httpProxy.createProxyServer({
-  target: targetUrl,
-  changeOrigin: true
-});
-
-// 创建 Express 应用程序
+// 创建一个express应用
 const app = express();
-
-// 添加反向代理规则
-app.use('/', (req, res) => {
-  // 将请求转发到反向代理服务器
-  proxy.web(req, res, { target: targetUrl });
-});
-
-// 启动应用程序
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+// 定义反向代理的目标网站
+const target = 'https://bing.cf03-b29.workers.dev';
+// 使用express-http-proxy中间件实现反向代理
+const proxy = require('express-http-proxy');
+// 将所有请求转发到目标网站
+app.use('/', proxy(target));
+// 监听3000端口
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
